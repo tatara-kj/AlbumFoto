c81d9a82f0090f0830972d213508afa69dbf4feb
Kuba Tataruch <130702084+tatara-kj@users.noreply.github.com>
2025-03-11 14:03:59 +0100

    Dodaj pliki projektów.

---
+++ AlbumFoto/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AlbumFoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.next = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AlbumFoto.Properties.Resources.europe1;
+            this.pictureBox1.InitialImage = global::AlbumFoto.Properties.Resources.europe1;
+            this.pictureBox1.Location = new System.Drawing.Point(410, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 343);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.next.Location = new System.Drawing.Point(983, 229);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(149, 81);
+            this.next.TabIndex = 1;
+            this.next.Text = "nastepny";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // prev
+            // 
+            this.prev.Location = new System.Drawing.Point(104, 229);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(149, 81);
+            this.prev.TabIndex = 2;
+            this.prev.Text = "poprzedni";
+            this.prev.UseVisualStyleBackColor = true;
+            this.prev.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 419);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(1035, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1262, 646);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button prev;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AlbumFoto/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbumFoto
+{
+    public partial class Form1: Form
+    {
+        private OpenFileDialog ofd;
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+       
+        private void zmien_obraz(int miejsce)
+        {
+            switch (miejsce)
+            {
+                case 0:
+                    pictureBox1.Image = Properties.Resources.europe1;
+                    break;
+                case 1:
+                    pictureBox1.Image = Properties.Resources.europe2;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.europe3;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.africa1;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.africa4;
+                    break;
+            }
+        }
+
+        private void zmien_napis(int miejsce)
+        {
+            switch (miejsce)
+            {
+                case 0:
+                    label1.Text = "Herkulanum to kolejne z miast zniszczonych podczas erupcji Wezuwiusza. W porównaniu do Pompejów tutejsze ruiny zachowały się w lepszym stanie, bo zostały zalane i zakonserwowane lawą, a nie pokryte popiołem.";
+                    break;
+                case 1:
+                    label1.Text = "Wieża Eiffla – najbardziej znany obiekt architektoniczny Paryża, uznawany za symbol tego miasta i niekiedy całej Francji. Jest najwyższą budowlą w Paryżu";
+                    break;
+                case 2:
+                    label1.Text = "Monumentalny gmach Reichstagu jest obecnie siedzibą drugiej izby niemieckiego Parlamentu. To również świadek trudniej historii kraju oraz istotny punkt na turystycznej mapie Berlina";
+                    break;
+                case 3:
+                    label1.Text = "Na zdjęciu widać różnorodną faunę Afryki – stado słoni przechodzi przez sawannę, a w oddali można dostrzec biegające zebry i gazele. Wszystko to w otoczeniu rozgrzanego, afrykańskiego krajobrazu.";
+                    break;
+                case 4:
+                    label1.Text = "Na zdjęciu widać grupę wielbłądów, które powoli przemierzają rozległą pustynię. Ich sylwetki rysują się na tle bezkresnego, piaszczystego krajobrazu, a w tle widać delikatne, falujące piaski pod wpływem gorącego słońca.";
+                    break;
+            }
+        }
+        int miejsce = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            miejsce = (miejsce +4) % 5;
+
+           zmien_obraz(miejsce);
+            zmien_napis(miejsce);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            miejsce = (miejsce + 6) % 5;
+            zmien_napis(miejsce);
+            zmien_obraz(miejsce);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog()== DialogResult.OK)
+            {
+                this.pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            this.pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        }
+    }
+}
